23b9bbb32a5b2bbb9c223d25685b1fb0532a08a9
Christian Scheb <privat@stylemotion.de>
2020-09-14 00:34:11 +0200

    Add hops pages

--- web_app/urls.py
+++ web_app/urls.py
@@ -17,12 +17,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('imprint', views.legal, name='legal'),
 
-    path('trends/', views.home, name='trends'),
-    path('hops/', views.home, name='hop_overview'),
-    path('fermentables/', views.home, name='fermentable_overview'),
-
     path('styles/', views.style_overview, name='style_overview'),
-    path('styles/<str:category_slug>', views.style_category_detail, name='style_category_detail'),
+    path('styles/<str:category_slug>/', views.style_category_detail, name='style_category_detail'),
     path('styles/<str:category_slug>/<str:slug>', views.style_detail, name='style_detail'),
     path('styles/charts/<str:id>/<str:chart_type>.<cformat:format>', views.style_chart, name='style_chart'),
+
+    path('hops/', views.hop_overview, name='hop_overview'),
+    path('hops/<str:category>/', views.hop_category_detail, name='hop_category_detail'),
+    path('hops/<str:category>/<str:slug>', views.hop_detail, name='hop_detail'),
+    path('hops/charts/<str:id>/<str:chart_type>.<cformat:format>', views.hop_chart, name='hop_chart'),
+
+    path('fermentables/', views.home, name='fermentable_overview'),
 ]
