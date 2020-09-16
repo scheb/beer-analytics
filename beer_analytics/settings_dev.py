2642d04bb89b6838dc31b1f74288695177b97cb1
Christian Scheb <privat@stylemotion.de>
2020-09-16 11:55:13 +0200

    Reorganize settings

---
+++ beer_analytics/settings_dev.py
@@ -0,0 +1,12 @@
+try:
+    from beer_analytics.settings_shared import *
+except ImportError:
+    pass
+
+DEBUG = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
