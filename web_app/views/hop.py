2ee5d85d15044e612ad1340fe25c26a9b7f07e11
Christian Scheb <privat@stylemotion.de>
2020-09-16 22:00:29 +0200

    Add hop/fermentable popularity and common styles

--- web_app/views/hop.py
+++ web_app/views/hop.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from recipe_db.models import Hop
+from web_app.charts.hop import HopChartFactory
 from web_app.views.utils import render_plot
 
 
@@ -49,5 +50,10 @@ def detail(request: HttpRequest, *args, **kwargs):
 def chart(request: HttpRequest, id: str, chart_type: str, format: str) -> HttpResponse:
     hop = get_object_or_404(Hop, pk=id)
 
-    raise Http404('Unknown chart type %s.' % chart_type)
+    chart_factory = HopChartFactory()
+    if chart_factory.is_supported_chart(chart_type):
+        plot = chart_factory.get_chart(hop, chart_type)
+    else:
+        raise Http404('Unknown chart type %s.' % chart_type)
+
     return render_plot(plot, format)
