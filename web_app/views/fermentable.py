6e0af0d690ed2292cff25fd478d48dc48e26cc30
Christian Scheb <privat@stylemotion.de>
2020-09-17 17:42:18 +0200

    Refactor fermentables typing, add types on category page

--- web_app/views/fermentable.py
+++ web_app/views/fermentable.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -7,42 +9,11 @@ from web_app.views.utils import render_plot
 
 
 def overview(request: HttpRequest) -> HttpResponse:
-    fermentable_categories = {}
-    categories = Fermentable.get_categories()
-    types = Fermentable.get_types()
-    for category in categories:
-        fermentable_types = {}
-        for t in types:
-            fermentable_types[t] = {
-                'id': t,
-                'name': types[t],
-                'fermentables': [],
-            }
-
-        fermentable_categories[category] = {
-            'id': category,
-            'name': categories[category],
-            'fermentables': [],
-            'types': fermentable_types,
-            'most_popular': []
-        }
-
     fermentables = Fermentable.objects.filter(recipes_count__gt=0).order_by('name')
-    for fermentable in fermentables:
-        if fermentable.type is not None:
-            fermentable_categories[fermentable.category]['types'][fermentable.type]['fermentables'].append(fermentable)
-        else:
-            fermentable_categories[fermentable.category]['fermentables'].append(fermentable)
-
-    fermentable_categories = fermentable_categories.values()
+    fermentable_categories = group_by_category(fermentables)
     for fermentable_category in fermentable_categories:
-        types_filtered = []
-        for t in fermentable_category['types'].values():
-            if len(t['fermentables']) > 0:
-                types_filtered.append(t)
-        fermentable_category['types'] = types_filtered
-        if len(fermentable_category['types']) > 1 or len(fermentable_category['fermentables']) > 5:
-            fermentable_category['most_popular'] = Fermentable.objects.filter(category=fermentable_category['id']).order_by('-recipes_count')[:5]
+        (fermentable_category['fermentables'], fermentable_category['types']) = group_by_type(fermentable_category['fermentables'])
+        fermentable_category['most_popular'] = Fermentable.objects.filter(category=fermentable_category['id']).order_by('-recipes_count')[:5]
 
     return render(request, 'fermentable/overview.html', {'categories': fermentable_categories})
 
@@ -52,16 +23,21 @@ def category(request: HttpRequest, category: str) -> HttpResponse:
     if category not in categories:
         raise Http404('Unknown fermentable category %s.' % category)
 
-    fermentables_query = Fermentable.objects.filter(category=category, recipes_count__gt=0)
-
-    fermentables = fermentables_query.order_by('name')
-    if len(fermentables) > 5:
-        most_popular = fermentables_query.order_by('-recipes_count')[:5]
-    else:
-        most_popular = []
     category_name = categories[category]
 
-    return render(request, 'fermentable/category.html', {'category_name': category_name, 'fermentables': fermentables, 'most_popular': most_popular})
+    fermentables_query = Fermentable.objects.filter(category=category, recipes_count__gt=0)
+    (fermentables, types) = group_by_type(fermentables_query.order_by('name'))
+
+    most_popular = []
+    if fermentables_query.count():
+        most_popular = fermentables_query.order_by('-recipes_count')[:5]
+
+    return render(request, 'fermentable/category.html', {
+        'category_name': category_name,
+        'fermentables': fermentables,
+        'types': types,
+        'most_popular': most_popular
+    })
 
 
 def detail(request: HttpRequest, slug: str, category: str) -> HttpResponse:
@@ -86,3 +62,58 @@ def chart(request: HttpRequest, id: str, chart_type: str, format: str) -> HttpRe
         raise Http404('Unknown chart type %s.' % chart_type)
 
     return render_plot(plot, format)
+
+
+def group_by_category(fermentables: iter) -> list:
+    fermentable_categories = {}
+    categories = Fermentable.get_categories()
+
+    # Create category object
+    for category in categories:
+        fermentable_categories[category] = {
+            'id': category,
+            'name': categories[category],
+            'fermentables': [],
+            'most_popular': []
+        }
+
+    # Assign fermentables
+    for fermentable in fermentables:
+        fermentable_categories[fermentable.category]['fermentables'].append(fermentable)
+
+    # Filter categories with fermentables
+    categories_filtered = []
+    for fermentable_category in fermentable_categories.values():
+        if len(fermentable_category['fermentables']):
+            categories_filtered.append(fermentable_category)
+
+    return categories_filtered
+
+
+def group_by_type(fermentables: iter) -> Tuple[list, list]:
+    untyped_fermentables = []
+    types = Fermentable.get_types()
+    fermentable_types = {}
+
+    # Create type objects
+    for t in types:
+        fermentable_types[t] = {
+            'id': t,
+            'name': types[t],
+            'fermentables': [],
+        }
+
+    # Assign fermentables (if possible)
+    for fermentable in fermentables:
+        if fermentable.type is not None:
+            fermentable_types[fermentable.type]['fermentables'].append(fermentable)
+        else:
+            untyped_fermentables.append(fermentable)
+
+    # Filter types with fermentables
+    types_filtered = []
+    for fermentable_type in fermentable_types.values():
+        if len(fermentable_type['fermentables']):
+            types_filtered.append(fermentable_type)
+
+    return untyped_fermentables, types_filtered
