faa3ba1064e3981c491aef1090bcab88d12cf6b1
Christian Scheb <privat@stylemotion.de>
2020-09-22 13:10:12 +0200

    Add tailing slashes

--- web_app/urls.py
+++ web_app/urls.py
@@ -18,23 +18,23 @@ register_converter(ChartFormat, 'cformat')
 
 urlpatterns = [
     path('', views.misc.home, name='home'),
-    path('legal', misc.legal, name='legal'),
-    path('about', misc.about, name='about'),
+    path('legal/', misc.legal, name='legal'),
+    path('about/', misc.about, name='about'),
     path('sitemap.xml', misc.sitemap, name='sitemap'),
 
     path('styles/', style.overview, name='style_overview'),
     path('styles/<str:category_slug>/', style.category, name='style_category'),
-    path('styles/<str:category_slug>/<str:slug>', style.detail, name='style_detail'),
+    path('styles/<str:category_slug>/<str:slug>/', style.detail, name='style_detail'),
     path('styles/charts/<str:id>/<str:chart_type>.<cformat:format>', style.chart, name='style_chart'),
 
     path('hops/', views.hop.overview, name='hop_overview'),
     path('hops/<str:category>/', hop.category, name='hop_category'),
-    path('hops/<str:category>/<str:slug>', hop.detail, name='hop_detail'),
+    path('hops/<str:category>/<str:slug>/', hop.detail, name='hop_detail'),
     path('hops/charts/<str:id>/<str:chart_type>.<cformat:format>', hop.chart, name='hop_chart'),
 
     path('fermentables/', fermentable.overview, name='fermentable_overview'),
     path('fermentables/<str:category>/', fermentable.category, name='fermentable_category'),
-    path('fermentables/<str:category>/<str:slug>', fermentable.detail, name='fermentable_detail'),
+    path('fermentables/<str:category>/<str:slug>/', fermentable.detail, name='fermentable_detail'),
     path('fermentables/charts/<str:id>/<str:chart_type>.<cformat:format>', fermentable.chart, name='fermentable_chart'),
 
     path('yeasts/', views.yeast.overview, name='yeast_overview'),
