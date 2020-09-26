0459f9d04d288cefd677e13b3851c9112bf1bf73
Christian Scheb <privat@stylemotion.de>
2020-09-26 14:13:51 +0200

    Downgrade JS to make minification work (for now)

--- web_app/static/js/application.js
+++ web_app/static/js/application.js
@@ -1,21 +1,23 @@
-function display_graph(container_id, chart_url, config = {}) {
+function display_graph(container_id, chart_url, config) {
+    config = (typeof config !== 'undefined') ?  config : {}
     display_graph_with_navigation(container_id, null, chart_url, config)
 }
 
-function display_graph_with_navigation(container_id, navigation_id, chart_url, config = {}) {
-    let container = document.getElementById(container_id)
+function display_graph_with_navigation(container_id, navigation_id, chart_url, config) {
+    config = (typeof config !== 'undefined') ?  config : {}
+    var container = document.getElementById(container_id)
     if (!container) {
         return
     }
 
-    let chart = new ChartMount(container, chart_url, config)
+    var chart = new ChartMount(container, chart_url, config)
 
     if (navigation_id) {
-        let navigation_tab_list = document.querySelectorAll('#' + navigation_id + ' a[data-toggle="tab"]')
+        var navigation_tab_list = document.querySelectorAll('#' + navigation_id + ' a[data-toggle="tab"]')
         navigation_tab_list.forEach(function (tab) {
             tab.addEventListener('shown.bs.tab', function (e) {
-                let filter = e.target.dataset['filter']
-                let query = {}
+                var filter = e.target.dataset['filter']
+                var query = {}
                 if (filter) {
                     query['filter'] = filter
                 }
@@ -27,54 +29,56 @@ function display_graph_with_navigation(container_id, navigation_id, chart_url, c
     chart.load()
 }
 
-class ChartMount {
-    constructor(container, chartUrl, chartConfig) {
-        this.current_request = null
-        this.container = container
-        this.chartUrl = chartUrl
-        let defaultConfig = {responsive: true, displayModeBar: true}
-        this.chartConfig = {...defaultConfig, ...chartConfig}
+function ChartMount(container, chartUrl, chartConfig) {
+    this.current_request = null
+    this.container = container
+    this.chartUrl = chartUrl
+
+    // Config defaults
+    this.chartConfig = chartConfig
+    this.chartConfig.responsive = this.chartConfig.responsive === "undefined" ? true : this.chartConfig.responsive
+    this.chartConfig.displayModeBar = this.chartConfig.displayModeBar === "undefined" ? true : this.chartConfig.displayModeBar
+}
+
+ChartMount.prototype.load = function(query) {
+    query = (typeof query !== 'undefined') ?  query : {}
+    if (null !== this.current_request) {
+        this.current_request.abort()
     }
 
-    load(query = {}) {
-        if (null !== this.current_request) {
-            this.current_request.abort()
-        }
+    this.container.innerHTML = '<div class="chart-loading"><span></span></div>'
 
-        this.container.innerHTML = '<div class="chart-loading"><span></span></div>'
+    var query_string = this.serializeFilter(query)
+    var url = this.chartUrl + (query_string ? '?'+query_string : '')
 
-        let query_string = this.serializeFilter(query)
-        let url = this.chartUrl + (query_string ? '?'+query_string : '')
-
-        let self = this
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                self.current_request = null
-                if (xhr.status === 204) {
-                    self.container.innerHTML = '<p class="no-data"><span>No data</span></p>'
-                } else if (xhr.status === 200) {
-                    self.container.innerHTML = '';
-                    let data = JSON.parse(xhr.responseText);
-                    Plotly.newPlot(self.container.id, data.data, data.layout, self.chartConfig)
-                } else {
-                    self.container.innerHTML = '<p class="no-data"><span>Failed loading data</span></p>'
-                }
+    var self = this
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            self.current_request = null
+            if (xhr.status === 204) {
+                self.container.innerHTML = '<p class="no-data"><span>No data</span></p>'
+            } else if (xhr.status === 200) {
+                self.container.innerHTML = '';
+                var data = JSON.parse(xhr.responseText);
+                Plotly.newPlot(self.container.id, data.data, data.layout, self.chartConfig)
+            } else {
+                self.container.innerHTML = '<p class="no-data"><span>Failed loading data</span></p>'
             }
         }
-
-        this.current_request = xhr
-        xhr.send();
     }
 
-    serializeFilter = function (obj) {
-        let str = [];
-        for (let p in obj) {
-            if (obj.hasOwnProperty(p)) {
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            }
+    this.current_request = xhr
+    xhr.send();
+}
+
+ChartMount.prototype.serializeFilter = function(obj) {
+    var str = [];
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
         }
-        return str.join("&");
     }
+    return str.join("&");
 }
