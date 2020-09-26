bcaa287c503b9e20604dd2cf13072a473cb18442
Christian Scheb <privat@stylemotion.de>
2020-09-26 12:43:22 +0200

    Add font

--- web_app/apps.py
+++ web_app/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.contrib.staticfiles.apps import StaticFilesConfig
 
 
 class WebAppConfig(AppConfig):
     name = 'web_app'
+
+
+class WebAppStaticFilesConfig(StaticFilesConfig):
+    ignore_patterns = ['CVS', '.*', '*~', 'LICENSE']
