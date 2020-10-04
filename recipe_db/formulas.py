5a2923e610cc6c55478b70e52f3f1787d5efd0b9
Christian Scheb <privat@stylemotion.de>
2020-10-04 22:20:49 +0200

    Add fahrenheit conversion

--- recipe_db/formulas.py
+++ recipe_db/formulas.py
@@ -111,3 +111,7 @@ def liters_to_gallons(liters: float) -> float:
 
 def fahrenheit_to_celsius(fahrenheit: float) -> float:
     return (fahrenheit - 32) * 5 / 9
+
+
+def celsius_to_fahrenheit(celsius: float) -> float:
+    return (celsius * 9 / 5) + 32
