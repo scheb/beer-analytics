905ad9b6cb0a41f6be644a959444af230a026dc6
Christian Scheb <privat@stylemotion.de>
2020-10-04 22:09:40 +0200

    Add yeast charts

--- web_app/charts/yeast.py
+++ web_app/charts/yeast.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+from recipe_db.analytics import get_yeast_popularity, get_yeast_common_styles_absolute, get_yeast_common_styles_relative
 from recipe_db.models import Yeast
 from web_app.charts.utils import NoDataException, Chart, ChartDefinition
 from web_app.plot import LinesChart, BarChart
