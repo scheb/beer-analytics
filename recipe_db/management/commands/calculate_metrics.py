dfa4290cd1a5e2eda2b407350cb92c3753189637
Christian Scheb <privat@stylemotion.de>
2020-10-04 22:31:47 +0200

    Add option to calculate only specific metrics

--- recipe_db/management/commands/calculate_metrics.py
+++ recipe_db/management/commands/calculate_metrics.py
@@ -17,75 +17,85 @@ FERMENTABLE_METRICS = ['amount_percent', 'color_lovibond', 'color_ebc']
 class Command(BaseCommand):
     help = "Calculate statistics on the recipes"
 
+    def add_arguments(self, parser):
+        parser.add_argument('--entities', '-e', nargs='+', type=str, help='Entities to recalculate')
+
     def handle(self, *args, **options) -> None:
-        recipes = load_all_recipes()
 
-        self.stdout.write('Calculate style stats')
-        for style in Style.objects.all():
-            self.stdout.write('Style {}'.format(style.name))
-            self.calculate_all_style_metrics(recipes, style)
-            style.save()
+        entities = options['entities'] or ['styles', 'hops', 'fermentables', 'yeasts']
 
-        recipe_hops = load_all_recipe_hops_aggregated()
+        if 'styles' in entities:
+            self.stdout.write('Calculate style stats')
+            recipes = load_all_recipes()
+            for style in Style.objects.all():
+                self.stdout.write('Style {}'.format(style.name))
+                self.calculate_all_style_metrics(recipes, style)
+                style.save()
 
-        self.stdout.write('Calculate hop stats')
-        for hop in Hop.objects.all():
-            self.stdout.write('Hop {}'.format(hop.name))
-            self.calculate_all_hop_metrics(recipe_hops, hop)
-            hop.save()
+        if 'hops' in entities:
+            recipe_hops = load_all_recipe_hops_aggregated()
+            self.stdout.write('Calculate hop stats')
+            for hop in Hop.objects.all():
+                self.stdout.write('Hop {}'.format(hop.name))
+                self.calculate_all_hop_metrics(recipe_hops, hop)
+                hop.save()
 
-        recipe_fermentables = load_all_recipe_fermentables_aggregated()
+        if 'fermentables' in entities:
+            recipe_fermentables = load_all_recipe_fermentables_aggregated()
+            self.stdout.write('Calculate fermentable stats')
+            for fermentable in Fermentable.objects.all():
+                self.stdout.write('Fermentable {}'.format(fermentable.name))
+                self.calculate_all_fermentable_metrics(recipe_fermentables, fermentable)
+                fermentable.save()
 
-        self.stdout.write('Calculate fermentable stats')
-        for fermentable in Fermentable.objects.all():
-            self.stdout.write('Fermentable {}'.format(fermentable.name))
-            self.calculate_all_fermentable_metrics(recipe_fermentables, fermentable)
-            fermentable.save()
-
-        recipe_yeasts = load_all_recipe_yeasts_aggregated()
-
-        self.stdout.write('Calculate yeast stats')
-        for yeast in Yeast.objects.all():
-            self.stdout.write('Yeast {}'.format(yeast.name))
-            self.calculate_all_yeast_metrics(recipe_yeasts, yeast)
-            yeast.save()
+        if 'yeasts' in entities:
+            recipe_yeasts = load_all_recipe_yeasts_aggregated()
+            self.stdout.write('Calculate yeast stats')
+            for yeast in Yeast.objects.all():
+                self.stdout.write('Yeast {}'.format(yeast.name))
+                self.calculate_all_yeast_metrics(recipe_yeasts, yeast)
+                yeast.save()
 
         # Recalculate percentiles
-        style_percentiles = get_style_percentiles()
-        for style in Style.objects.all():
-            self.stdout.write('Calculate percentile for style {}'.format(style.name))
-            style.recipes_percentile = 0
-            if style.id in style_percentiles:
-                style.recipes_percentile = style_percentiles[style.id]['percentile']
-                style.save()
-            self.stdout.write(str(style.recipes_percentile))
+        if 'styles' in entities:
+            style_percentiles = get_style_percentiles()
+            for style in Style.objects.all():
+                self.stdout.write('Calculate percentile for style {}'.format(style.name))
+                style.recipes_percentile = 0
+                if style.id in style_percentiles:
+                    style.recipes_percentile = style_percentiles[style.id]['percentile']
+                    style.save()
+                self.stdout.write(str(style.recipes_percentile))
 
-        hop_percentiles = get_hops_percentiles()
-        for hop in Hop.objects.all():
-            self.stdout.write('Calculate percentile for hop {}'.format(hop.name))
-            hop.recipes_percentile = 0
-            if hop.id in hop_percentiles:
-                hop.recipes_percentile = hop_percentiles[hop.id]['percentile']
-                hop.save()
-            self.stdout.write(str(hop.recipes_percentile))
+        if 'hops' in entities:
+            hop_percentiles = get_hops_percentiles()
+            for hop in Hop.objects.all():
+                self.stdout.write('Calculate percentile for hop {}'.format(hop.name))
+                hop.recipes_percentile = 0
+                if hop.id in hop_percentiles:
+                    hop.recipes_percentile = hop_percentiles[hop.id]['percentile']
+                    hop.save()
+                self.stdout.write(str(hop.recipes_percentile))
 
-        fermentable_percentiles = get_fermentables_percentiles()
-        for fermentable in Fermentable.objects.all():
-            self.stdout.write('Calculate percentile for fermentable {}'.format(fermentable.name))
-            fermentable.recipes_percentile = 0
-            if fermentable.id in fermentable_percentiles:
-                fermentable.recipes_percentile = fermentable_percentiles[fermentable.id]['percentile']
-                fermentable.save()
-            self.stdout.write(str(fermentable.recipes_percentile))
+        if 'fermentables' in entities:
+            fermentable_percentiles = get_fermentables_percentiles()
+            for fermentable in Fermentable.objects.all():
+                self.stdout.write('Calculate percentile for fermentable {}'.format(fermentable.name))
+                fermentable.recipes_percentile = 0
+                if fermentable.id in fermentable_percentiles:
+                    fermentable.recipes_percentile = fermentable_percentiles[fermentable.id]['percentile']
+                    fermentable.save()
+                self.stdout.write(str(fermentable.recipes_percentile))
 
-        yeast_percentiles = get_yeasts_percentiles()
-        for yeast in Yeast.objects.all():
-            self.stdout.write('Calculate percentile for yeast {}'.format(yeast.name))
-            yeast.recipes_percentile = 0
-            if yeast.id in yeast_percentiles:
-                yeast.recipes_percentile = yeast_percentiles[yeast.id]['percentile']
-                yeast.save()
-            self.stdout.write(str(yeast.recipes_percentile))
+        if 'yeasts' in entities:
+            yeast_percentiles = get_yeasts_percentiles()
+            for yeast in Yeast.objects.all():
+                self.stdout.write('Calculate percentile for yeast {}'.format(yeast.name))
+                yeast.recipes_percentile = 0
+                if yeast.id in yeast_percentiles:
+                    yeast.recipes_percentile = yeast_percentiles[yeast.id]['percentile']
+                    yeast.save()
+                self.stdout.write(str(yeast.recipes_percentile))
 
     def calculate_all_style_metrics(self, df, style: Style) -> None:
         style.recipes_count = calculate_style_recipe_count(df, style)
