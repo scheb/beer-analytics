35b1dcfe570f1ee879bdffcb58916342bd31defa
Christian Scheb <privat@stylemotion.de>
2020-10-12 12:31:04 +0200

    Fix selection of styles in trend chart

--- recipe_db/analytics/charts/trend.py
+++ recipe_db/analytics/charts/trend.py
@@ -3,13 +3,12 @@ from typing import List
 from pandas import DataFrame
 
 from recipe_db.analytics.analysis import RecipesPopularityAnalysis
-from recipe_db.analytics.scope import RecipeScope
+from recipe_db.analytics.scope import RecipeScope, StyleProjection
 from recipe_db.models import Style
 
 
 def get_specific_styles_popularity(styles: List[Style]) -> DataFrame:
-    scope = RecipeScope()
-    scope.style_ids = styles
-    scope.include_sub_styles = False  # Only analyze the styles provided
-    analysis = RecipesPopularityAnalysis(scope)
-    return analysis.popularity_per_style()
+    projection = StyleProjection()
+    projection.styles = styles
+    analysis = RecipesPopularityAnalysis(RecipeScope())
+    return analysis.popularity_per_style(projection)
