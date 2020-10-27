d9f0a6fb39d787d6403640c0fe007ec7177e70f2
Christian Scheb <privat@stylemotion.de>
2020-10-27 21:54:48 +0100

    Migrate asset pipeline to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        app: "./web_app/static/js/application.js",
+        style: "./web_app/static/scss/beer_analytics.scss"
+    },
+    output: {
+        path: path.resolve(__dirname, "./bundles"),
+        publicPath: "/static/",
+        filename: "[name].js",
+        chunkFilename: "[id]-[chunkhash].js",
+    },
+    devServer: {
+        port: 8081,
+        writeToDisk: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(svg)$/,
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        limit: 5120,
+                    },
+                },
+            },
+            {
+                test: /\.(woff|woff2)/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "fonts"
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                        }
+                    },
+                    'extract-loader',
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ],
+    },
+};
