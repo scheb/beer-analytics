ac100e77560e07cb79f8238e0e3d7de14c813fd5
Christian Scheb <privat@stylemotion.de>
2020-11-06 16:28:42 +0100

    Remove unnecessary transformation to array

--- web_app/static/ts/application.ts
+++ web_app/static/ts/application.ts
@@ -4,18 +4,18 @@ import {Chart, Recipes} from "./results";
 
 document.addEventListener("DOMContentLoaded", function() {
     // Init tabs
-    Array.from(document.querySelectorAll('.tabs'))
+    document.querySelectorAll('.tabs')
         .forEach(tabNode => new Tab(tabNode))
 
     // Init detail lists
-    Array.from(document.querySelectorAll("dl.detail-info"))
+    document.querySelectorAll("dl.detail-info")
         .forEach(listNode => new DetailList(listNode))
 
     // Init charts
-    Array.from(document.querySelectorAll("[data-chart]"))
+    document.querySelectorAll("[data-chart]")
         .forEach(chartNode => new Chart(chartNode))
 
     // Init recipes lists
-    Array.from(document.querySelectorAll("[data-recipes]"))
+    document.querySelectorAll("[data-recipes]")
         .forEach(recipesNode => new Recipes(recipesNode))
 })
