7fecfa18ebe69fdb44b056e8fe5b55dfb527761f
Christian Scheb <privat@stylemotion.de>
2023-04-23 15:09:35 +0200

    Add working influx chart

--- web_app/charts/admin.py
+++ web_app/charts/admin.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
+import datetime
 from abc import ABC
-from enum import Enum
 from typing import Optional
 
-from recipe_db.analytics.recipe import RecipesPopularityAnalysis, RecipesTrendAnalysis
-from recipe_db.analytics.scope import RecipeScope, HopProjection, YeastProjection
-from recipe_db.analytics.spotlight.hop import HOP_FILTER_TO_USES
-from recipe_db.analytics.spotlight.yeast import YEAST_FILTER_TO_TYPES
+from recipe_db.analytics.recipe import RecipesPopularityAnalysis
+from recipe_db.analytics.scope import RecipeScope
+from recipe_db.analytics.utils import months_ago
 from web_app.charts.utils import Chart, ChartDefinition, NoDataException
 from web_app.plot import LinesChart
 
@@ -26,13 +25,16 @@ class InfluxSourceChart(ChartDefinition, ABC):
         return self.IMAGE_ALT
 
     def plot(self) -> Chart:
-        analysis = RecipesPopularityAnalysis(RecipeScope())
-        df = analysis.popularity_per_source(top_months=24)
+        scope = RecipeScope()
+        scope.creation_date_min = months_ago(6).strftime("%Y-%m-%d")
+        scope.creation_date_max = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+        analysis = RecipesPopularityAnalysis(scope)
+        df = analysis.popularity_per_source()
         if len(df) <= 1:  # 1, because a single data point is also meaningless
             raise NoDataException()
 
         figure = LinesChart(force_legend=True).plot(
-            df, "month", "recipes_number", "source", "Month/Year", "Number Recipes"
+            df, "day", "recipes_number", "source", "Day", "Number Recipes"
         )
         return Chart(figure, height=Chart.DEFAULT_HEIGHT * 0.66, title=self.get_chart_title())
 
