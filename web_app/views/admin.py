ebb863f5a2d92c2d00ad17358aa35620ba0fccad
Christian Scheb <privat@stylemotion.de>
2023-05-07 17:38:06 +0200

    Description checker

--- web_app/views/admin.py
+++ web_app/views/admin.py
@@ -5,10 +5,11 @@ from django.views.decorators.cache import cache_page
 from recipe_db.analytics.fermentable import UnmappedFermentablesAnalysis
 from recipe_db.analytics.hop import UnmappedHopsAnalysis
 from recipe_db.analytics.yeast import UnmappedYeastsAnalysis
+from recipe_db.models import Hop, Tag
 from web_app import DEFAULT_PAGE_CACHE_TIME
 from web_app.charts.admin import AdminChartFactory
 from web_app.charts.utils import NoDataException
-from web_app.views.utils import render_chart
+from web_app.views.utils import render_chart, template_exists
 
 
 @cache_page(DEFAULT_PAGE_CACHE_TIME, cache="default")
@@ -37,3 +38,30 @@ def chart(request: HttpRequest, chart_type: str, format: str) -> HttpResponse:
         raise Http404("Unknown chart type %s." % chart_type)
 
     return render_chart(chart, format)
+
+
+@cache_page(DEFAULT_PAGE_CACHE_TIME, cache="data")
+def descriptions(request: HttpRequest) -> HttpResponse:
+    hop_descriptions = []
+    flavor_descriptions = []
+
+    for hop in Hop.objects.all().filter(recipes_count__gt=0).order_by('-recipes_count'):
+        hop_descriptions.append({
+            'name': hop.name,
+            'recipes_count': hop.recipes_count,
+            'has_description': template_exists("hop/descriptions/hops/%s.html" % hop.id),
+        })
+
+    for flavor in Tag.objects.all().order_by('name'):
+        flavor_descriptions.append({
+            'name': flavor.name,
+            'hops_count': flavor.accessible_hops_count,
+            'has_description': template_exists("hop/descriptions/flavors/%s.html" % flavor.id),
+        })
+
+    context = {
+        'hops': hop_descriptions,
+        'flavors': sorted(flavor_descriptions, key=lambda f: f['hops_count'], reverse=True),
+    }
+
+    return render(request, "admin/descriptions.html", context)
