4348832fdd012a6286c496712f1865d17a4c7fb1
Christian Scheb <privat@stylemotion.de>
2023-05-13 10:49:50 +0200

    Extend description admin

--- web_app/views/admin.py
+++ web_app/views/admin.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 from recipe_db.analytics.fermentable import UnmappedFermentablesAnalysis
 from recipe_db.analytics.hop import UnmappedHopsAnalysis
 from recipe_db.analytics.yeast import UnmappedYeastsAnalysis
-from recipe_db.models import Hop, Tag
+from recipe_db.models import Hop, Tag, Yeast
 from web_app import DEFAULT_PAGE_CACHE_TIME
 from web_app.charts.admin import AdminChartFactory
 from web_app.charts.utils import NoDataException
@@ -44,6 +44,7 @@ def chart(request: HttpRequest, chart_type: str, format: str) -> HttpResponse:
 def descriptions(request: HttpRequest) -> HttpResponse:
     hop_descriptions = []
     flavor_descriptions = []
+    yeast_descriptions = []
 
     for hop in Hop.objects.all().filter(recipes_count__gt=0).order_by('-recipes_count'):
         hop_descriptions.append({
@@ -59,9 +60,17 @@ def descriptions(request: HttpRequest) -> HttpResponse:
             'has_description': template_exists("hop/descriptions/flavors/%s.html" % flavor.id),
         })
 
+    for yeast in Yeast.objects.all().order_by('-recipes_count'):
+        yeast_descriptions.append({
+            'name': yeast.product_name,
+            'recipes_count': yeast.recipes_count,
+            'has_description': template_exists("yeast/descriptions/%s.html" % yeast.id),
+        })
+
     context = {
         'hops': hop_descriptions,
         'flavors': sorted(flavor_descriptions, key=lambda f: f['hops_count'], reverse=True),
+        'yeasts': yeast_descriptions,
     }
 
     return render(request, "admin/descriptions.html", context)
