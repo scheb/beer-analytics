74aea585616549755d5888976892d57de9cce517
Christian Scheb <privat@stylemotion.de>
2023-12-10 18:29:16 +0100

    Increase number of records kept in cache

--- beer_analytics/settings_prod.py
+++ beer_analytics/settings_prod.py
@@ -5,10 +5,22 @@ except ImportError:
 
 DEBUG = False
 
+NUM_ENTITIES = 1500
 CACHES = {
-    "default": {"BACKEND": "django.core.cache.backends.filebased.FileBasedCache", "LOCATION": path.join(env.str("CACHE_DIR"), "default")},
-    "data": {"BACKEND": "django.core.cache.backends.filebased.FileBasedCache", "LOCATION": path.join(env.str("CACHE_DIR"), "data")},
-    "images": {"BACKEND": "django.core.cache.backends.filebased.FileBasedCache", "LOCATION": path.join(env.str("CACHE_DIR"), "images")},
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": path.join(env.str("CACHE_DIR"), "default"),
+        "OPTIONS": {"MAX_ENTRIES": 3 * NUM_ENTITIES},
+    },
+    "data": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": path.join(env.str("CACHE_DIR"), "data"),
+        "OPTIONS": {"MAX_ENTRIES": 3 * NUM_ENTITIES * 10},
+     },
+    "images": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": path.join(env.str("CACHE_DIR"), "images")},
+        "OPTIONS": {"MAX_ENTRIES": 3 * NUM_ENTITIES * 10},
 }
 
 SESSION_COOKIE_SECURE = True
