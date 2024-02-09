1007a52aecc933673a7c326a8d037ee0fbff662b
Christian Scheb <privat@stylemotion.de>
2024-02-09 22:03:07 +0100

    Build with ASGI

--- build/production/entrypoint.sh
+++ build/production/entrypoint.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Collect all static images
 python manage.py collectstatic --no-input
 
-exec python3 manage.py runserver 0.0.0.0:8000
+exec /usr/local/bin/gunicorn config.asgi --bind 0.0.0.0:5000 --chdir=/app -k uvicorn.workers.UvicornWorker
