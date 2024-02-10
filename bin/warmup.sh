a85097fd126cce2d8cc8946b71ee2786180753b1
Christian Scheb <privat@stylemotion.de>
2024-02-10 14:07:36 +0100

    Remove dependency on virtualenv

--- bin/warmup.sh
+++ bin/warmup.sh
@@ -5,9 +5,4 @@ set -o pipefail
 
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
-# Activate virtualenv
-source ${APP_DIR}/venv/bin/activate
-
 python ${APP_DIR}/manage.py warmup_cache
-
-deactivate
