03293e03a321e1627ed365e0cf8e19e547eaab0c
Christian Scheb <privat@stylemotion.de>
2025-02-16 17:01:03 +0100

    Update homepage hops

--- web_app/views/misc.py
+++ web_app/views/misc.py
@@ -12,7 +12,7 @@ from web_app.charts.style import StyleChartFactory
 from web_app.charts.yeast import YeastChartFactory
 from web_app.meta import PageMeta, HomeMeta
 
-FEATURED_HOPS = ['elani', 'nectaron', 'luminosa', 'vic-secret', 'hbc-1019', 'lotus']
+FEATURED_HOPS = ['anchovy', 'nectaron', 'elani', 'hbc-1019', 'motueka', 'krush']
 
 @cache_page(DEFAULT_PAGE_CACHE_TIME, cache="default")
 def home(request: HttpRequest) -> HttpResponse:
